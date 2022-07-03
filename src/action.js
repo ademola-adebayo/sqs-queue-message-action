9695121020ce2c4d33641aa666aeca8058ce28b2
Demola <ademoladebayo@gmail.com>
2022-07-03 18:54:42 +0100

    Check GA

--- src/action.js
+++ src/action.js
@@ -2,42 +2,55 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const AWS = require("aws-sdk");
 
+
+const AWS_ACCESS_KEY_ID = core.getInput("AWS_ACCESS_KEY_ID", { require: true });
+const AWS_SECRET_ACCESS_KEY = core.getInput("AWS_SECRET_ACCESS_KEY", { require: true });
+const AWS_REGION = core.getInput("AWS_REGION", { require: true });
+const QUEUE_NAME = core.getInput("QUEUE_NAME", { require: true });
+const ATTRIBUTE_NAMES = core.getInput("ATTRIBUTE_NAMES");
+
+AWS.config.update({
+  region: AWS_REGION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY
+});
+
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+
+async function getQueues() {
+  var queueParams = {
+    QueueName: QUEUE_NAME
+  };
+  try {
+    const result = await sqs.getQueueUrl(queueParams).promise()
+    return result.QueueUrl
+  } catch (err) {
+    console.log(`err ${err.message}`)
+  }
+}
+
 async function run() {
   try {
-    const AWS_ACCESS_KEY_ID = core.getInput("AWS_ACCESS_KEY_ID", { require: true });
-    const AWS_SECRET_ACCESS_KEY = core.getInput("AWS_SECRET_ACCESS_KEY", { require: true });
-    const AWS_REGION = core.getInput("AWS_REGION", { require: true });
-    const QUEUE_NAME = core.getInput("QUEUE_NAME", { require: true });
-    const ATTRIBUTE_NAMES = core.getInput("ATTRIBUTE_NAMES");
 
-    AWS.config.update({
-      region: AWS_REGION,
-      accessKeyId: AWS_ACCESS_KEY_ID,
-      secretAccessKey: AWS_SECRET_ACCESS_KEY
-    });
+   const returnedUrl = getQueues()
+   console.log(`RETURNED ${returnedUrl}`); 
 
-    var queueParams = {
-      QueueName: QUEUE_NAME
-    };
+   
 
-    const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+  //  sqs.getQueueUrl(queueParams, (err, data) => {
+  //     if (err) {
+  //       core.debug(err.Message);
+  //       core.setFailed(err.Message); 
+  //     } else {
+  //       console.log(`resp ${JSON.stringify(data, null, 2)}`);
+  //       const { ResponseMetadata, QueueUrl } = data
+  //       return { 
+  //         ResponseMetadata,
+  //         QueueUrl
+  //       }
+  //     }
+  //   });
 
-    const { ResponseMetadata, QueueUrl } =  sqs.getQueueUrl(queueParams, (err, data) => {
-      if (err) {
-        core.debug(err.Message);
-        core.setFailed(err.Message); 
-      } else {
-        console.log(`resp ${JSON.stringify(data, null, 2)}`);
-        const { ResponseMetadata, QueueUrl } = data
-        return { 
-          ResponseMetadata,
-          QueueUrl
-        }
-      }
-    });
-
-    console.log(`QUEUE NAME => ${QueueUrl}`)
-    console.log(`METADATA => ${ResponseMetadata}`)
   } catch (error) {
     core.setFailed(error.message);
   }
